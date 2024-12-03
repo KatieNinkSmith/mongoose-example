efb0a59e2a7abcf9d502e7c8cf6bc5fb4e24c7fd
Katie Nink Smith <katiej.smith@outlook.com>
2024-12-03 13:04:27 -0700

    server is running and connected to database

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import db from "./db/conn.mjs";
+
+const PORT = process.env.PORT || 5052;
+
+const app = express();
+
+app.use(express.json());
+
+// start the express server
+app.listen(PORT, () => {
+  console.log(`Listen Linda, Listen on port:${PORT}`);
+});
